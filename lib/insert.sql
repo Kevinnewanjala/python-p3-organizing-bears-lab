14abfa72c3e3a274483f38d19c6ebc739254e0ec
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-05-31 16:23:23 +0300

    First commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 10, 'M', 'brown', 'calm', TRUE),
+(2, 'Rowdy', 5, 'M', 'black', 'aggressive', TRUE),
+(3, 'Tabitha', 7, 'F', 'brown', 'friendly', TRUE),
+(4, 'Sergeant Brown', 8, 'M', 'brown', 'calm', TRUE),
+(5, 'Melissa', 4, 'F', 'blonde', 'shy', TRUE),
+(6, 'Grinch', 6, 'M', 'green', 'grumpy', TRUE),
+(7, 'Wendy', 3, 'F', 'white', 'playful', TRUE),
+(8, NULL, 2, 'F', 'brown', 'unknown', FALSE);
